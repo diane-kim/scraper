b51de1f72f6b6681194c39a81213f6a51908909c
diane.k <diane.k@kakaopaycorp.com>
2020-03-20 10:34:31 +0900

    remove krx find roe if not exist expectation

--- naver/stock.py
+++ naver/stock.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from multiprocessing import Pool
 f = open("codes.txt", 'r')
 target = open("data.txt", 'w', 1)
-target.write("종목	코드	현재가	시가총액	W_PER	K_PER	E_PER	PBR	ROE 배당수익률\n")
+target.write("종목	코드	현재가	시가총액	PER	E_PER	PBR	ROE	배당수익률\n\n")
 
 
 def safe_list_get_text(element_list, index):
@@ -28,23 +28,30 @@ def scrap(code):
     title = safe_list_get_text(soup.select('#middle > div.h_company > div.wrap_company > h2 > a'), 0)
     price = strip_exclude_number(safe_list_get_text(
         soup.select("#content > div.section.invest_trend > div.sub_section.right > table > tbody  em"), 0).strip())
-    tot = strip_exclude_number(safe_list_get_text(soup.select("#_market_sum"), 0).strip())
+    tot = ''
+    totstr = safe_list_get_text(soup.select("#_market_sum"), 0).strip().split("조")
+    if len(totstr) > 1:
+        tot = totstr[0].strip() + totstr[1].strip().zfill(4)
+    else:
+        tot = totstr[0].strip()
     per1 = safe_list_get_text(soup.select("#_per"), 0).strip()
-    per2 = safe_list_get_text(soup.select("#krx_per"), 0).strip()
+    #per2 = safe_list_get_text(soup.select("#krx_per"), 0).strip()
     per3 = safe_list_get_text(soup.select("#_cns_per"), 0).strip()
     pbr = safe_list_get_text(soup.select("#_pbr"), 0).strip()
     try:
         roe = soup.select(".cop_analysis > .sub_section > table > tbody > tr")[5].select("td")[3].get_text().strip()
+        if not roe:
+            roe = soup.select(".cop_analysis > .sub_section > table > tbody > tr")[5].select("td")[2].get_text().strip()
     except IndexError:
         roe = ""
-        print("no roe")
+        print(f'{code} no roe')
     dvr = safe_list_get_text(soup.select("#_dvr"), 0).strip()
     data.append(title)
     data.append(code)
     data.append(price)
     data.append(tot)
     data.append(per1)
-    data.append(per2)
+    #data.append(per2)
     data.append(per3)
     data.append(pbr)
     data.append(roe)
